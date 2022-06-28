762a2c5113e6f359e1129b28f1b711998bc9dc48
Ryuta Iwakura <ryuuta.iwakura@gmail.com>
2022-06-29 08:13:31 +0900

    Migration to 1.19

--- src/main/java/net/unknown/survival/enchants/nms/EnchantExample.java
+++ src/main/java/net/unknown/survival/enchants/nms/EnchantExample.java
@@ -33,16 +33,13 @@ package net.unknown.survival.enchants.nms;
 
 import net.minecraft.Util;
 import net.minecraft.core.Registry;
-import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.MobType;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
-import net.minecraft.world.item.enchantment.EnchantmentHelper;
-
-import java.util.Random;
 
 public class EnchantExample extends Enchantment {
     public static void register() {
@@ -76,8 +73,8 @@ public class EnchantExample extends Enchantment {
     @Override
     public void doPostAttack(LivingEntity user, Entity entity, int level) {
         if(entity instanceof LivingEntity target) {
-            user.sendMessage(new TextComponent(":eyes:"), Util.NIL_UUID);
-            target.sendMessage(new TextComponent(":eyes:"), Util.NIL_UUID);
+            user.sendSystemMessage(Component.literal(":eyes:"));
+            target.sendSystemMessage(Component.literal((":eyes:")));
         }
     }
 }
