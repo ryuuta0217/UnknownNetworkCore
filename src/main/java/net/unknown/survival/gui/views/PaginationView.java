17cbcec64cc1c5c672e65e344394589b9fbd204e
Ryuta Iwakura <ryuuta.iwakura@gmail.com>
2022-08-25 17:10:17 +0900

    [WIP] PaginationView

--- src/main/java/net/unknown/survival/gui/views/PaginationView.java
+++ src/main/java/net/unknown/survival/gui/views/PaginationView.java
@@ -31,13 +31,69 @@
 
 package net.unknown.survival.gui.views;
 
+import com.ryuuta0217.util.ListUtil;
+import net.unknown.core.gui.GuiBase;
 import net.unknown.core.gui.View;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemStack;
 
-public class PaginationView implements View {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class PaginationView<T> implements View {
+    private final GuiBase gui;
+    private final Set<T> data;
+    private final List<Set<T>> splitData;
+    private final Function<T, ItemStack> processor;
+    private final Map<Integer, T> slot2data = new HashMap<>();
+
+    private int currentPage = 1;
+
+    public PaginationView(GuiBase gui, Set<T> data, Function<T, ItemStack> processor, BiConsumer<InventoryClickEvent, T> onClick) {
+        this.gui = gui;
+        this.data = data;
+        this.splitData = ListUtil.splitListAsLinkedSet(data, 45);
+        this.processor = processor;
+    }
+
     @Override
     public void initialize() {
-        
+        this.showPage(1);
+    }
+
+    private void showPage(int newPage) {
+        if (this.splitData.size() >= newPage) throw new IllegalArgumentException("Maximum allowed page " + this.splitData.size() + " reached.");
+        this.currentPage = newPage;
+        this.clearInventory();
+        this.splitData.get(this.currentPage - 1).forEach(data -> {
+            int slot = this.gui.getInventory().firstEmpty();
+            if (slot != -1) {
+                ItemStack item = processor.apply(data);
+                this.gui.getInventory().setItem(slot, item);
+                slot2data.put(slot, data);
+            }
+        });
+
+        if (this.splitData.size() > 1) {
+            if (this.currentPage > 1) {
+                // Previous button
+            } else {
+                // Remove Previous button
+                this.gui.getInventory().clear(52);
+            }
+
+            if (this.currentPage <= this.splitData.size()) {
+                // Next button
+            } else {
+                // Remove Next button
+                this.gui.getInventory().clear(53);
+            }
+        }
     }
 
     @Override
@@ -47,6 +103,6 @@ public class PaginationView implements View {
 
     @Override
     public void clearInventory() {
-
+        this.slot2data.keySet().forEach(slot -> this.gui.getInventory().clear(slot));
     }
 }
