c80e0ad2ff9395f53f94012196c8f93757a57773
Ryuta Iwakura <ryuuta.iwakura@gmail.com>
2022-08-27 22:00:45 +0900

    PrefixesView is out

--- src/main/java/net/unknown/survival/gui/prefix/PrefixGui.java
+++ src/main/java/net/unknown/survival/gui/prefix/PrefixGui.java
@@ -3,12 +3,11 @@ package net.unknown.survival.gui.prefix;
 import net.kyori.adventure.text.Component;
 import net.unknown.core.define.DefinedTextColor;
 import net.unknown.core.gui.GuiBase;
-import net.unknown.core.gui.view.ChooseView;
 import net.unknown.core.gui.view.View;
-import net.unknown.core.prefix.PlayerPrefixes;
 import net.unknown.survival.gui.MainGui;
 import net.unknown.survival.gui.prefix.view.PrefixesView;
 import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryCloseEvent;
 
 import javax.annotation.Nonnull;
 
@@ -22,9 +21,14 @@ public class PrefixGui extends GuiBase {
         super(opener, 54, Component.text("接頭辞/接尾辞", DefinedTextColor.DARK_GREEN), false);
         this.parent = parent;
         this.opener = opener;
+        this.view = new PrefixesView(this, opener);
+        this.state = PrefixGuiState.AVAILABLE_PREFIXES;
+    }
 
-        ChooseView.Builder builder = ChooseView.Builder.newBuilder(this);
-        
-        this.view = builder.build();
+    @Override
+    public void onClose(InventoryCloseEvent event) {
+        if (this.state != PrefixGuiState.WAITING_CALLBACK) {
+            this.unRegisterAsListener();
+        }
     }
 }
--- src/main/java/net/unknown/survival/gui/prefix/view/PrefixesView.java
+++ src/main/java/net/unknown/survival/gui/prefix/view/PrefixesView.java
@@ -1,4 +1,27 @@
 package net.unknown.survival.gui.prefix.view;
 
-public class PrefixesView {
+import net.kyori.adventure.text.Component;
+import net.unknown.core.builder.ItemStackBuilder;
+import net.unknown.core.prefix.PlayerPrefixes;
+import net.unknown.core.util.NewMessageUtil;
+import net.unknown.survival.gui.prefix.PrefixGui;
+import net.unknown.core.gui.view.PaginationView;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+
+public class PrefixesView extends PaginationView<Component> {
+    public PrefixesView(PrefixGui gui, Player player) {
+        super(gui, PlayerPrefixes.getAvailablePrefixes(player),
+                (prefix) -> new ItemStackBuilder(Material.NAME_TAG).displayName(prefix).build(),
+                (event, prefix) -> {
+                    PlayerPrefixes.setPrefix(event.getWhoClicked().getUniqueId(), prefix);
+                    event.getWhoClicked().closeInventory();
+                    NewMessageUtil.sendMessage(((Player) event.getWhoClicked()), Component.empty()
+                            .append(Component.text("接頭辞を "))
+                            .append(prefix)
+                            .append(Component.text(" に変更しました")));
+                },
+                (e) -> {
+                });
+    }
 }
