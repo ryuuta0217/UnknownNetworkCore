1216888073d6ced54f7b310cbda329babe90ec37
Ryuta Iwakura <ryuuta.iwakura@gmail.com>
2022-08-31 07:52:29 +0900

    [PrefixesView] とりあえず動いてる

--- src/main/java/net/unknown/survival/gui/prefix/view/PrefixesView.java
+++ src/main/java/net/unknown/survival/gui/prefix/view/PrefixesView.java
@@ -1,3 +1,34 @@
+/*
+ * Copyright (c) 2022 Unknown Network Developers and contributors.
+ *
+ * All rights reserved.
+ *
+ * NOTICE: This license is subject to change without prior notice.
+ *
+ * Redistribution and use in source and binary forms, *without modification*,
+ *     are permitted provided that the following conditions are met:
+ *
+ * I. Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *
+ * II. Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ * III. Neither the name of Unknown Network nor the names of its contributors may be used to
+ *     endorse or promote products derived from this software without specific prior written permission.
+ *
+ * IV. This source code and binaries is provided by the copyright holders and contributors "AS-IS" and
+ *     any express or implied warranties, including, but not limited to, the implied warranties of
+ *     merchantability and fitness for a particular purpose are disclaimed.
+ *     In not event shall the copyright owner or contributors be liable for
+ *     any direct, indirect, incidental, special, exemplary, or consequential damages
+ *     (including but not limited to procurement of substitute goods or services;
+ *     loss of use data or profits; or business interruption) however caused and on any theory of liability,
+ *     whether in contract, strict liability, or tort (including negligence or otherwise)
+ *     arising in any way out of the use of this source code, event if advised of the possibility of such damage.
+ */
+
 package net.unknown.survival.gui.prefix.view;
 
 import net.kyori.adventure.text.Component;
@@ -25,17 +56,30 @@ public class PrefixesView extends PaginationView<Component> {
                             .append(prefix)
                             .append(Component.text(" に変更しました")));
                 },
-                (event, instance) -> {
-            gui.setState(PrefixGuiState.WAITING_CALLBACK);
-            new SignGui()
-                    .withTarget(player)
-                    .withLines(Component.empty(), Component.text("^^^^^^^^^^^^^^^^^^^^"), Component.text("接頭辞を入力"), Component.empty())
-                    .onComplete(lines -> {
-                        String raw = PlainTextComponentSerializer.plainText().serialize(lines.get(0));
-                        Component colored = LegacyComponentSerializer.legacyAmpersand().deserialize(raw);
-                        PlayerPrefixes.addAvailablePrefix(player, colored);
-                        instance.setData(PlayerPrefixes.getAvailablePrefixes(player), true);
-                    }).open();
+                (event, view) -> {
+                    gui.setState(PrefixGuiState.WAITING_CALLBACK);
+                    new SignGui()
+                            .withTarget(player)
+                            .withLines(Component.empty(),
+                                    Component.text("^^^^^^^^^^^^^^^^^^^^"),
+                                    Component.text("接頭辞を入力"),
+                                    Component.text("\"&r \"が最後ﾆ挿入ｻﾚﾏｽ"))
+                            .onComplete(lines -> {
+                                String raw = PlainTextComponentSerializer.plainText().serialize(lines.get(0));
+                                if (!raw.isEmpty()) {
+                                    raw += "&r ";
+                                    Component colored = LegacyComponentSerializer.legacyAmpersand().deserialize(raw);
+                                    PlayerPrefixes.addAvailablePrefix(player, colored);
+                                    NewMessageUtil.sendMessage(((Player) event.getWhoClicked()), Component.empty()
+                                            .append(Component.text("接頭辞 "))
+                                            .append(colored)
+                                            .append(Component.text("を追加しました"))); // "&r " が挿入されているはずなので、スペースは無し TODO: (ChatManagerのほうでスペースを挿入するか？)
+                                }
+                                player.openInventory(gui.getInventory());
+                                gui.setState(PrefixGuiState.AVAILABLE_PREFIXES);
+                                view.setData(PlayerPrefixes.getAvailablePrefixes(player), true);
+                            }).open();
                 });
+        this.initialize();
     }
 }
