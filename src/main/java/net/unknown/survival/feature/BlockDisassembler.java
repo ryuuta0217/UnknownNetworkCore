0f4748fee2f77e686c843cab7bd809ff2a34a31c
Ryuta Iwakura <ryuuta.iwakura@gmail.com>
2023-05-13 15:28:20 +0900

    BlockDisassembler is now working

--- src/main/java/net/unknown/survival/feature/BlockDisassembler.java
+++ src/main/java/net/unknown/survival/feature/BlockDisassembler.java
@@ -31,16 +31,50 @@
 
 package net.unknown.survival.feature;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.DispenserBlock;
+import net.minecraft.world.level.block.LevelEvent;
 import net.minecraft.world.level.block.entity.DispenserBlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.gameevent.GameEvent;
 import net.unknown.launchwrapper.event.BlockDispenseBeforeEvent;
-import org.bukkit.block.Dispenser;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+
+import java.util.List;
 
 public class BlockDisassembler implements Listener {
     @EventHandler
     public void onDispenserShoot(BlockDispenseBeforeEvent event) {
-        DispenserBlockEntity dispenser = event.getBlockSource().getEntity();
-        Dispenser dispenserBukkit = (Dispenser) event.getBukkitBlock().getState(); // Dropperの場合もあるぞ！
+        if (event.getBlockSource().getBlockState().getBlock() == Blocks.DISPENSER && event.getBlockSource().getEntity() instanceof DispenserBlockEntity dispenser) {
+            if (dispenser.getDisplayName().contains(Component.literal("Block Disassembler").withStyle(ChatFormatting.RED, ChatFormatting.BOLD))) {
+                ServerLevel level = ((ServerLevel) dispenser.getLevel());
+                BlockPos pos = dispenser.getBlockPos();
+                Direction direction = dispenser.getBlockState().getValue(DispenserBlock.FACING);
+                BlockPos targetPos = pos.relative(direction);
+                ItemStack shootItem = event.getItem();
+                shootItem.hurt(1, level.random, null);
+                event.setCancelled(true);
+                destroyBlockWithDrops(level, targetPos, shootItem).forEach(dropItem -> {
+                    Block.popResource(level, targetPos, dropItem);
+                });
+            }
+        }
+    }
+
+    public static List<ItemStack> destroyBlockWithDrops(ServerLevel level, BlockPos blockPos, ItemStack useItem) {
+        BlockState currentState = level.getBlockState(blockPos);
+        level.levelEvent(LevelEvent.PARTICLES_DESTROY_BLOCK, blockPos, Block.getId(currentState));
+        List<ItemStack> drops = Block.getDrops(currentState, level, blockPos, currentState.hasBlockEntity() ? level.getBlockEntity(blockPos) : null, null, useItem);
+        level.setBlock(blockPos, Blocks.AIR.defaultBlockState(), Block.UPDATE_ALL, 512);
+        level.gameEvent(GameEvent.BLOCK_DESTROY, blockPos, GameEvent.Context.of(null, currentState));
+        return drops;
     }
 }
