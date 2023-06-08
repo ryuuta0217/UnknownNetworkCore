e40ffcfacc2bc24f54a5e9c044ca8b4dc0b99049
Ryuta Iwakura <ryuuta.iwakura@gmail.com>
2023-06-08 22:27:10 +0900

    fix compile error - [WIP] Discord Integration

--- src/main/java/net/unknown/survival/UnknownNetworkSurvival.java
+++ src/main/java/net/unknown/survival/UnknownNetworkSurvival.java
@@ -43,6 +43,7 @@ import net.unknown.survival.commands.Commands;
 import net.unknown.survival.data.PlayerData;
 import net.unknown.survival.data.Warps;
 import net.unknown.survival.dependency.WorldGuard;
+import net.unknown.survival.discord.MinecraftToDiscordMessageListener;
 import net.unknown.survival.enchants.*;
 import net.unknown.survival.enchants.nms.DamageEnchant;
 import net.unknown.survival.events.ModifiableBlockBreakEvent;
@@ -120,7 +121,7 @@ public class UnknownNetworkSurvival {
         ListenerManager.registerListener(new ServerRestartListener());
         ListenerManager.registerListener(new ProtectedAreaTestStick());
         ListenerManager.registerListener(new PlayerJoinListener());
-        ListenerManager.registerListener(new DiscordAudience.MinecraftListener());
+        ListenerManager.registerListener(new MinecraftToDiscordMessageListener());
         //ListenerManager.registerListener(new WorldSeparator());
         if (isBootstrapped()) {
             getLogger().info("Successfully Bootstrapped!");
