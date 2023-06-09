30a08926c20eb3ee3278d35274a81c8ac1205ca7
Ryuta Iwakura <ryuuta.iwakura@gmail.com>
2023-06-10 01:22:05 +0900

    Add VoteTicket

--- src/main/java/net/unknown/survival/feature/VoteListener.java
+++ src/main/java/net/unknown/survival/feature/VoteListener.java
@@ -36,23 +36,73 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.event.ClickEvent;
 import net.kyori.adventure.text.format.Style;
 import net.kyori.adventure.text.format.TextDecoration;
+import net.unknown.core.builder.ItemStackBuilder;
 import net.unknown.core.define.DefinedTextColor;
-import net.unknown.core.util.MessageUtil;
-import net.unknown.core.util.NewMessageUtil;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
 import org.bukkit.event.*;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataType;
+
+import java.util.*;
+import java.util.logging.Logger;
 
 public class VoteListener implements Listener {
+    private static final Logger LOGGER = Logger.getLogger("UNC/Vote");
+    private static final Map<UUID, Set<ItemStack>> INSERT_ITEM_QUEUE = new HashMap<>();
+
     @EventHandler
     public void onVote(VotifierEvent event) {
         Bukkit.broadcast(Component.empty()
                 .append(Component.text("[Unknown System]", Style.style(DefinedTextColor.GOLD, TextDecoration.BOLD)))
                 .append(Component.space())
                 .append(Component.text(event.getVote().getUsername() + " さんが投票しました。ありがとう！", DefinedTextColor.GREEN)));
+
+        OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(Bukkit.getPlayerUniqueId(event.getVote().getUsername()));
+
+        ItemStack ticket = new ItemStackBuilder(Material.PAPER)
+                .displayName(Component.text("投票チケット", DefinedTextColor.GOLD).decorate(TextDecoration.BOLD))
+                .addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 1)
+                .addItemFlag(ItemFlag.HIDE_ENCHANTS)
+                .lore(Component.text("投票でもらえるチケット", DefinedTextColor.AQUA).decorate(TextDecoration.BOLD))
+                .custom(stack -> {
+                    ItemMeta meta = stack.getItemMeta();
+                    meta.getPersistentDataContainer().set(NamespacedKey.fromString("unknown-network:voted_player"), PersistentDataType.STRING, offlinePlayer.getUniqueId().toString());
+                    stack.setItemMeta(meta);
+                })
+                .build();
+
+        if (offlinePlayer.isOnline()) {
+            Player player = Bukkit.getPlayer(event.getVote().getUsername());
+            player.getInventory().addItem(ticket).forEach((slot, item) -> player.getWorld().dropItem(player.getLocation(), item));
+        } else {
+            if (INSERT_ITEM_QUEUE.containsKey(offlinePlayer.getUniqueId())) INSERT_ITEM_QUEUE.put(offlinePlayer.getUniqueId(), new HashSet<>());
+            INSERT_ITEM_QUEUE.get(offlinePlayer.getUniqueId()).add(ticket);
+            LOGGER.info("Give vote ticket queued. Currently queued " + INSERT_ITEM_QUEUE.size() + " player(s).");
+        }
         Bukkit.broadcast(Component.text("あなたも投票してみませんか？ ", DefinedTextColor.GREEN)
                 .append(Component.text("投票する: ", DefinedTextColor.AQUA)
                         .append(Component.text("monocraft", Style.style(DefinedTextColor.AQUA, TextDecoration.UNDERLINED)).clickEvent(ClickEvent.openUrl("https://monocraft.net/servers/hWvNPIBskVkZ743kWt8S/vote")))
                         .append(Component.space())
                         .append(Component.text("JMS", Style.style(DefinedTextColor.AQUA, TextDecoration.UNDERLINED)).clickEvent(ClickEvent.openUrl("https://minecraft.jp/servers/play.mc-unknown.net/vote")))));
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        if (INSERT_ITEM_QUEUE.containsKey(event.getPlayer().getUniqueId())) {
+            INSERT_ITEM_QUEUE.get(event.getPlayer().getUniqueId()).forEach(queuedStack -> {
+                event.getPlayer().getInventory().addItem(queuedStack).forEach((slot, stack) -> {
+                    event.getPlayer().getWorld().dropItem(event.getPlayer().getLocation(), stack);
+                });
+            });
+            INSERT_ITEM_QUEUE.remove(event.getPlayer().getUniqueId());
+        }
     }
 }
