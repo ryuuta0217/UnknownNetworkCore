9f3b1f227b510bb0607ec19fc2ceb1e65c160f6e
Ryuta Iwakura <ryuuta.iwakura@gmail.com>
2023-06-26 02:16:54 +0900

    MVCore: Level returns Location

--- src/main/java/net/unknown/survival/dependency/MultiverseCore.java
+++ src/main/java/net/unknown/survival/dependency/MultiverseCore.java
@@ -1,7 +1,9 @@
 package net.unknown.survival.dependency;
 
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec2;
 import net.minecraft.world.phys.Vec3;
+import net.royawesome.jlibnoise.module.combiner.Min;
 import net.unknown.core.util.MinecraftAdapter;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
@@ -19,9 +21,15 @@ public class MultiverseCore {
         return getInstance().getMVWorldManager().getMVWorld(world).getSpawnLocation();
     }
 
-    public static Vec3 getSpawnLocation(Level level) {
-        if (!MULTIVERSE_CORE_ENABLED) return level.getSharedSpawnPos().getCenter();
-        return MinecraftAdapter.vec3(getInstance().getMVWorldManager().getMVWorld(MinecraftAdapter.world(level)).getSpawnLocation());
+    public static Location getSpawnLocation(Level level) {
+        Vec3 positionVector3 = level.getSharedSpawnPos().getCenter();
+        Vec2 rotationVector2 = new Vec2(level.getSharedSpawnAngle(), 0);
+        if (MULTIVERSE_CORE_ENABLED) {
+            Location multiverseSpawnLocation = getSpawnLocation(MinecraftAdapter.world(level));
+            positionVector3 = MinecraftAdapter.vec3(multiverseSpawnLocation);
+            rotationVector2 = MinecraftAdapter.vec2(multiverseSpawnLocation);
+        }
+        return MinecraftAdapter.location(level, positionVector3, rotationVector2);
     }
 
     public static com.onarandombox.MultiverseCore.MultiverseCore getInstance() {
