b4b60beeaa670f74e1d18f5f70f9b409192390e4
Ryuta Iwakura <ryuuta.iwakura@gmail.com>
2023-06-26 09:39:08 +0900

    feat: /spawn configure mode <...>

--- src/main/java/net/unknown/survival/commands/SpawnCommand.java
+++ src/main/java/net/unknown/survival/commands/SpawnCommand.java
@@ -33,21 +33,134 @@ package net.unknown.survival.commands;
 
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.event.HoverEvent;
 import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.unknown.core.managers.RunnableManager;
 import net.unknown.core.util.MessageUtil;
+import net.unknown.core.util.NewMessageUtil;
+import net.unknown.survival.data.PlayerData;
+import net.unknown.survival.dependency.MultiverseCore;
 import net.unknown.survival.enums.Permissions;
-import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.NamespacedKey;
 import org.bukkit.event.player.PlayerTeleportEvent;
 
+import java.util.Set;
+
 public class SpawnCommand {
+    private static final NamespacedKey SPAWN_COMMAND_REGISTRY_KEY = new NamespacedKey("unknown-network", "spawn_command");
+
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
         LiteralArgumentBuilder<CommandSourceStack> builder = LiteralArgumentBuilder.literal("spawn");
-        builder.requires(Permissions.COMMAND_SPAWN::checkAndIsPlayer);
+        builder.requires(Permissions.COMMAND_SPAWN::check);
         builder.executes(ctx -> {
-            ctx.getSource().getBukkitEntity().teleportAsync(Bukkit.getWorld("world").getSpawnLocation(), PlayerTeleportEvent.TeleportCause.COMMAND);
-            MessageUtil.sendMessage(ctx.getSource(), "スポーンにテレポートしました");
+            Entity executor = ctx.getSource().getEntityOrException();
+            ServerLevel currentLevel = (ServerLevel) executor.level();
+            ServerLevel whereToSpawn = MinecraftServer.getServer().getLevel(Level.OVERWORLD); // fallback
+
+            switch (getSpawnCommandMode(ctx.getSource())) {
+                case TELEPORT_TO_MAIN_WORLD_SPAWN -> whereToSpawn = MinecraftServer.getServer().getLevel(Level.OVERWORLD);
+                case TELEPORT_TO_CURRENT_WORLD_SPAWN -> whereToSpawn = currentLevel;
+                case TELEPORT_TO_MAIN_WORLD_WHEN_IN_CURRENT_WORLD_SPAWN -> {
+                    Location currentPos = ctx.getSource().getBukkitEntity().getLocation();
+                    Location currentWorldSpawn = MultiverseCore.getSpawnLocation(ctx.getSource().getLevel());
+                    if (currentWorldSpawn.distance(currentPos) <= 3) {
+                        whereToSpawn = MinecraftServer.getServer().getLevel(Level.OVERWORLD);
+                    } else {
+                        whereToSpawn = ctx.getSource().getLevel();
+                    }
+                }
+            }
+
+            Location whereToTeleport = MultiverseCore.getSpawnLocation(whereToSpawn);
+            ctx.getSource().getEntityOrException().teleportTo(whereToSpawn, whereToTeleport.getX(), whereToTeleport.getY(), whereToTeleport.getZ(), Set.of(), whereToTeleport.getYaw(), whereToTeleport.getPitch(), PlayerTeleportEvent.TeleportCause.COMMAND);
+            NewMessageUtil.sendMessage(ctx.getSource(), Component.empty()
+                    .append(Component.text(MessageUtil.getWorldName(whereToSpawn.getWorld())))
+                    .append(Component.text("ワールドのスポーン地点にテレポートしました")));
             return 0;
         });
+
+        LiteralArgumentBuilder<CommandSourceStack> configureModeNode = Commands.literal("mode")
+                .executes(ctx -> {
+                    SpawnCommandMode mode = getSpawnCommandMode(ctx.getSource());
+                    NewMessageUtil.sendMessage(ctx.getSource(), Component.empty()
+                            .append(Component.text("スポーンコマンドの動作モードは "))
+                            .append(Component.text(mode.getLiteral()).hoverEvent(HoverEvent.showText(mode.getDescription())))
+                            .append(Component.text(" に設定されています。")));
+                    return 0;
+                });
+
+        for (SpawnCommandMode mode : SpawnCommandMode.values()) {
+            configureModeNode.then(Commands.literal(mode.getLiteral())
+                    .executes(ctx -> {
+                        setSpawnCommandMode(ctx.getSource(), mode);
+                        NewMessageUtil.sendMessage(ctx.getSource(), Component.empty()
+                                .append(Component.text("スポーンコマンドの動作モードを変更しました: "))
+                                .append(mode.getDescription()));
+                        return 0;
+                    }));
+        }
+
+        builder.then(
+                Commands.literal("configure")
+                        .requires(Permissions.COMMAND_SPAWN::checkAndIsPlayer)
+                        .then(configureModeNode)
+        );
+
         dispatcher.register(builder);
+    }
+
+    private static SpawnCommandMode getSpawnCommandMode(CommandSourceStack source) throws CommandSyntaxException {
+        SpawnCommandMode mode = SpawnCommandMode.TELEPORT_TO_MAIN_WORLD_SPAWN;
+        if (source.getEntity() instanceof Player player) { // CommandSourceStack#getEntity を使用することで、/execute as .. で実行された場合に、対象となるプレイヤーのデータを参照する
+            PlayerData pd = PlayerData.of(player.getUUID());
+            PlayerData.PlayerRegistry pr = pd.getRegistries();
+            if (pr.containsKey(SPAWN_COMMAND_REGISTRY_KEY, "mode")) {
+                try {
+                    mode = SpawnCommandMode.valueOf(pr.get(SPAWN_COMMAND_REGISTRY_KEY, "mode"));
+                } catch (Throwable ignored) {
+                } // config <mode> で更新されれば治るはずなので握りつぶします。そもそも(SpawnCommandModeが増えたり消えたりしない限りは)到達しないはず...
+            }
+        }
+        return mode;
+    }
+
+    private static void setSpawnCommandMode(CommandSourceStack source, SpawnCommandMode mode) throws CommandSyntaxException {
+        if (source.getEntity() instanceof Player player) {
+            PlayerData pd = PlayerData.of(player.getUUID());
+            PlayerData.PlayerRegistry pr = pd.getRegistries();
+            pr.put(SPAWN_COMMAND_REGISTRY_KEY, "mode", mode.name());
+            RunnableManager.runAsync(pd::save);
+        }
+    }
+
+    public enum SpawnCommandMode {
+        TELEPORT_TO_MAIN_WORLD_SPAWN("teleportToMainWorldSpawn", Component.text("常にメインワールドのスポーン地点にテレポートします")),
+        TELEPORT_TO_CURRENT_WORLD_SPAWN("teleportToCurrentWorldSpawn", Component.text("常に実行時点でのワールドのスポーン地点にテレポートします")),
+        TELEPORT_TO_MAIN_WORLD_WHEN_IN_CURRENT_WORLD_SPAWN("teleportToMainWorldWhenInCurrentWorldSpawn", Component.text("実行時点でのワールドのスポーン地点周辺にいる場合は、メインワールドにテレポートします。"));
+
+        private final String literal;
+        private final Component description;
+
+        SpawnCommandMode(String literal, Component description) {
+            this.literal = literal;
+            this.description = description;
+        }
+
+        public String getLiteral() {
+            return literal;
+        }
+
+        public Component getDescription() {
+            return description;
+        }
     }
 }
