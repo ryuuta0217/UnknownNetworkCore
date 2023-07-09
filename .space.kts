d67495755a7ef61446cda4cb78abe85229a6b9d6
Ryuta Iwakura <ryuuta.iwakura@gmail.com>
2023-07-09 20:43:18 +0900

    Use Amazon Corretto for Build in Space

--- .space.kts
+++ .space.kts
@@ -5,7 +5,7 @@
 */
 
 job("Build") {
-  container(displayName = "Run gradle build", image = "openjdk:17") {
+  container(displayName = "Run gradle build", image = "amazoncorretto:17") { // Using Amazon Corretto 17 to avoids the xargs not present error.
     env["CI"] = "true" // for paperweight, do not decompile re-mapped minecraft jar, to increase performance (execution time --;) and reduce memory usage
     kotlinScript { api ->
       api.gradlew("clean downloadWaterfall reCompressWaterfall compileJava jar shadowJar reobfJar")
