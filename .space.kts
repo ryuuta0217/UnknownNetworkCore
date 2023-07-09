fa00429398661539cc381091971b6054f242e91e
Ryuta Iwakura <ryuuta.iwakura@gmail.com>
2023-07-09 21:09:30 +0900

    When pushed to repository to run job for space

--- .space.kts
+++ .space.kts
@@ -5,6 +5,11 @@
 */
 
 job("Build") {
+  startOn {
+    gitPush {
+      enabled = true
+    }
+  }
   container(displayName = "Run gradle build", image = "amazoncorretto:17") { // Using Amazon Corretto 17 to avoids the xargs not present error.
     env["CI"] = "true" // for paperweight, do not decompile re-mapped minecraft jar, to increase performance (execution time --;) and reduce memory usage
     kotlinScript { api ->
