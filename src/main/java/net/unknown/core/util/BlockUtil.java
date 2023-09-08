686faa21e353f52a6120cab2476c9fe19d0991f2
Ryuta Iwakura <ryuuta.iwakura@gmail.com>
2023-09-08 13:29:15 +0900

    [BlockUtil] Remove unused methods

--- src/main/java/net/unknown/core/util/BlockUtil.java
+++ src/main/java/net/unknown/core/util/BlockUtil.java
@@ -31,26 +31,11 @@
 
 package net.unknown.core.util;
 
-import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
-import net.minecraft.network.protocol.game.ClientboundBlockUpdatePacket;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.item.ItemEntity;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.*;
-import net.minecraft.world.level.block.entity.BeehiveBlockEntity;
-import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import org.bukkit.craftbukkit.v1_20_R1.block.CraftBlock;
-import org.bukkit.craftbukkit.v1_20_R1.event.CraftEventFactory;
-import org.bukkit.event.block.BlockBreakEvent;
-
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class BlockUtil {
@@ -68,122 +53,5 @@ public class BlockUtil {
 
         // 周囲ブロックの捜索が全て終了したあと、さらに捜索を続行する(forEachのなかで行ってしまうと、同じ方向ばかりを捜索してしまうため。)
         foundPositions.forEach(foundPos -> searchBlock(foundPos, maxBlockCount, level, searchTarget, data));
-    }
-
-    public static void searchBlockWithinManhattan(BlockPos center, int rangeX, int rangeY, int rangeZ, Level level, Block searchTarget, Set<BlockPos> data) {
-        if (searchTarget == Blocks.AIR) return;
-        BlockPos.withinManhattan(center, rangeX, rangeY, rangeZ).forEach(pos -> {
-            BlockPos immPos = pos.immutable();
-            BlockState state = level.getBlockStateIfLoaded(immPos);
-            if (state != null && state.is(searchTarget) && !data.contains(immPos) && !center.equals(immPos)) {
-                data.add(immPos);
-            }
-        });
-    }
-
-    public static void destroyBlockWithNoEvent(ServerPlayer player, ServerLevel level, BlockPos pos, ItemStack usedTool) {
-        BlockState blockState = level.getBlockState(pos);
-        BlockEntity blockEntity = level.getBlockEntity(pos);
-        Block block = blockState.getBlock();
-
-        if (block instanceof GameMasterBlock && !player.canUseGameMasterBlocks() && !player.isCreative() && !(block instanceof CommandBlock && player.getBukkitEntity().hasPermission("minecraft.commandblock"))) {
-            level.sendBlockUpdated(pos, blockState, blockState, Block.UPDATE_ALL);
-        } else {
-            if (player.blockActionRestricted(level, pos, player.gameMode.getGameModeForPlayer())) return;
-
-            block.playerWillDestroy(level, pos, blockState, player);
-            boolean hasRemoved = level.removeBlock(pos, false);
-
-            if (hasRemoved) {
-                block.destroy(level, pos, blockState);
-            }
-
-            boolean isCorrectTool = player.hasCorrectToolForDrops(blockState);
-
-            if (!player.isCreative()) {
-                usedTool.mineBlock(level, blockState, pos, player);
-
-                if (hasRemoved && isCorrectTool) {
-                    block.playerDestroy(level, player, pos, blockState, blockEntity, usedTool);
-                    block.popExperience(level, pos, block.getExpDrop(blockState, level, pos, usedTool, true), player);
-                }
-            }
-
-            if (hasRemoved && isCorrectTool && block instanceof BeehiveBlock && blockEntity instanceof BeehiveBlockEntity hive) {
-                CriteriaTriggers.BEE_NEST_DESTROYED.trigger(player, blockState, usedTool, hive.getOccupantCount());
-            }
-        }
-    }
-
-    public static void destroyBlock(ServerPlayer player, ServerLevel level, BlockPos pos, ItemStack usedTool) {
-        org.bukkit.block.Block bukkitBlock = CraftBlock.at(level, pos);
-        BlockBreakEvent event = new BlockBreakEvent(bukkitBlock, player.getBukkitEntity());
-
-        BlockState blockState = level.getBlockState(pos);
-        Block block = blockState.getBlock();
-
-        if (!event.isCancelled() && !player.isCreative() && player.hasCorrectToolForDrops(blockState)) {
-            event.setExpToDrop(block.getExpDrop(blockState, level, pos, usedTool, true));
-        }
-
-        level.getCraftServer().getPluginManager().callEvent(event);
-
-        if (event.isCancelled()) {
-            player.connection.send(new ClientboundBlockUpdatePacket(level, pos));
-
-            for (Direction direction : Direction.values()) {
-                player.connection.send(new ClientboundBlockUpdatePacket(level, pos.relative(direction)));
-            }
-
-            BlockEntity blockEntity = level.getBlockEntity(pos);
-            if (blockEntity != null) {
-                player.connection.send(blockEntity.getUpdatePacket());
-            }
-
-            return;
-        }
-
-        blockState = level.getBlockState(pos); // CraftBukkit - update state from plugins
-        if (blockState.isAir()) return; // CraftBukkit - A plugin set block to air without cancelling
-        BlockEntity blockEntity = level.getBlockEntity(pos);
-        block = blockState.getBlock();
-
-        if (block instanceof GameMasterBlock && !player.canUseGameMasterBlocks() && !player.isCreative() && !(block instanceof CommandBlock && player.getBukkitEntity().hasPermission("minecraft.commandblock"))) {
-            level.sendBlockUpdated(pos, blockState, blockState, Block.UPDATE_ALL);
-        } else {
-            if (player.blockActionRestricted(level, pos, player.gameMode.getGameModeForPlayer())) return;
-
-            org.bukkit.block.BlockState bukkitBlockState = bukkitBlock.getState();
-            level.captureDrops = new ArrayList<>();
-            block.playerWillDestroy(level, pos, blockState, player);
-
-            boolean removed = level.removeBlock(pos, false);
-
-            if (removed) {
-                block.destroy(level, pos, blockState);
-            }
-
-            ItemStack usedToolCopy = usedTool.copy();
-            boolean isCorrectTool = !blockState.requiresCorrectToolForDrops() || usedTool.isCorrectToolForDrops(blockState);
-
-            if (!player.isCreative()) { // Player is not creative mode
-                usedTool.mineBlock(level, blockState, pos, player); // awardStat() to UsedTool
-                if (removed && isCorrectTool && event.isDropItems()) { // Successfully removed block, is correct tool, dropItems true.
-                    block.playerDestroy(level, player, pos, blockState, blockEntity, usedToolCopy); // awardStat() to Block, hungry, popResource
-                }
-            }
-
-            List<ItemEntity> itemsToDrop = level.captureDrops;
-            level.captureDrops = null;
-            if (event.isDropItems()) {
-                CraftEventFactory.handleBlockDropItemEvent(bukkitBlock, bukkitBlockState, player, itemsToDrop);
-            }
-
-            blockState.getBlock().popExperience(level, pos, event.getExpToDrop(), player);
-
-            if (isCorrectTool && event.isDropItems() && block instanceof BeehiveBlock && blockEntity instanceof BeehiveBlockEntity beeHive) {
-                CriteriaTriggers.BEE_NEST_DESTROYED.trigger(player, blockState, usedToolCopy, beeHive.getOccupantCount());
-            }
-        }
     }
 }
