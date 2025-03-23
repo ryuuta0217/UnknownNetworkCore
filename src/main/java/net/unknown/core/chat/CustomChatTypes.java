533910650cfd9c6d60eb8353bb07f622b83d18f5
Ryuta Iwakura <ryuuta.iwakura@gmail.com>
2025-03-24 00:15:40 +0900

    Fix ChatType: incorrect argument descriptor

--- src/main/java/net/unknown/core/chat/CustomChatTypes.java
+++ src/main/java/net/unknown/core/chat/CustomChatTypes.java
@@ -62,11 +62,11 @@ public class CustomChatTypes {
     public static final ChatType VALUE_PRIVATE_MESSAGE_OUTGOING = new ChatType(
             new ChatTypeDecoration( // for chat view
                     "§b[PM]§r [→ %s] %s",
-                    List.of(ChatTypeDecoration.Parameter.SENDER, ChatTypeDecoration.Parameter.CONTENT),
+                    List.of(ChatTypeDecoration.Parameter.TARGET, ChatTypeDecoration.Parameter.CONTENT),
                     Style.EMPTY),
             new ChatTypeDecoration( // for narration
                     "あなたが %s に %s と言いました",
-                    List.of(ChatTypeDecoration.Parameter.SENDER, ChatTypeDecoration.Parameter.CONTENT),
+                    List.of(ChatTypeDecoration.Parameter.TARGET, ChatTypeDecoration.Parameter.CONTENT),
                     Style.EMPTY));
     public static final net.kyori.adventure.chat.ChatType PRIVATE_MESSAGE_OUTGOING_ADVENTURE = net.kyori.adventure.chat.ChatType.chatType(NamespacedKey.fromString(PRIVATE_MESSAGE_OUTGOING.location().toString()));
 
