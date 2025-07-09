ea89c86ec57dccc080304ec2db38f2d10a5c7b17
Ryuta Iwakura <ryuuta.iwakura@gmail.com>
2025-07-09 16:58:35 +0900

    MinecraftAdapter.ItemStack: Fix for parsing

--- src/main/java/net/unknown/core/util/MinecraftAdapter.java
+++ src/main/java/net/unknown/core/util/MinecraftAdapter.java
@@ -33,11 +33,18 @@ package net.unknown.core.util;
 
 import ca.spottedleaf.dataconverter.minecraft.MCDataConverter;
 import ca.spottedleaf.dataconverter.minecraft.MCVersions;
+import ca.spottedleaf.dataconverter.minecraft.datatypes.MCDataType;
 import ca.spottedleaf.dataconverter.minecraft.datatypes.MCTypeRegistry;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.datafixers.DataFix;
+import com.mojang.serialization.Dynamic;
 import com.mojang.serialization.JsonOps;
 import net.kyori.adventure.chat.SignedMessage;
 import net.kyori.adventure.key.Key;
+import net.minecraft.commands.arguments.item.ItemArgument;
 import net.minecraft.core.Direction;
 import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
@@ -51,6 +58,8 @@ import net.minecraft.network.chat.PlayerChatMessage;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.datafix.DataFixers;
+import net.minecraft.util.datafix.fixes.References;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec2;
@@ -188,33 +197,25 @@ public class MinecraftAdapter {
     public static class ItemStack {
         @Nullable
         public static net.minecraft.world.item.ItemStack json(String json) {
-            try {
-                CompoundTag tag = TagParser.parseCompoundFully(json);
-                return tag(tag);
-            } catch (CommandSyntaxException e) {
-                return null;
-            }
+            return net.minecraft.world.item.ItemStack.CODEC.decode(MinecraftServer.getServer().registryAccess().createSerializationContext(JsonOps.INSTANCE), DataFixers.getDataFixer().update(References.ITEM_STACK, new Dynamic<>(MinecraftServer.getServer().registryAccess().createSerializationContext(JsonOps.INSTANCE), JsonParser.parseString(json)), MCVersions.V1_21_4, MCVersions.V1_21_7).getValue()).getOrThrow().getFirst();
         }
 
         @Nonnull
         public static String json(net.minecraft.world.item.ItemStack itemStack) {
-            return net.minecraft.world.item.ItemStack.CODEC.encodeStart(JsonOps.INSTANCE, itemStack).getOrThrow().toString();
+            return net.minecraft.world.item.ItemStack.CODEC.encodeStart(MinecraftServer.getServer().registryAccess().createSerializationContext(JsonOps.INSTANCE), itemStack).getOrThrow().toString();
         }
 
         public static String json(org.bukkit.inventory.ItemStack bukkit) {
             return json(itemStack(bukkit));
         }
 
-        public static net.minecraft.world.item.ItemStack tag(CompoundTag tag) {
-            if (!tag.contains("components") && tag.contains("tag")) {
-                // Need to convert old NBT format to new format (DataComponent)
-                tag = MCDataConverter.convertTag(MCTypeRegistry.ITEM_STACK, tag, MCVersions.V1_20_4, MCVersions.V1_21_7);
-            }
-            return net.minecraft.world.item.ItemStack.CODEC.parse(NbtOps.INSTANCE, tag).getOrThrow();
+        public static net.minecraft.world.item.ItemStack tag(Tag tag) {
+            tag = DataFixers.getDataFixer().update(References.ITEM_STACK, new Dynamic<>(MinecraftServer.getServer().registryAccess().createSerializationContext(NbtOps.INSTANCE), tag), MCVersions.V1_21_4, MCVersions.V1_21_7).getValue();
+            return net.minecraft.world.item.ItemStack.CODEC.parse(MinecraftServer.getServer().registryAccess().createSerializationContext(NbtOps.INSTANCE), tag).getOrThrow();
         }
 
         public static Tag tag(net.minecraft.world.item.ItemStack itemStack) {
-            return net.minecraft.world.item.ItemStack.CODEC.encodeStart(NbtOps.INSTANCE, itemStack).getOrThrow();
+            return net.minecraft.world.item.ItemStack.CODEC.encodeStart(MinecraftServer.getServer().registryAccess().createSerializationContext(NbtOps.INSTANCE), itemStack).getOrThrow();
         }
 
         public static net.minecraft.world.item.ItemStack itemStack(org.bukkit.inventory.ItemStack bukkit) {
