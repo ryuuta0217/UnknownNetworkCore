bac6844f4f5cc3baf5658e6b48f4abda338db132
Ryuta Iwakura <ryuuta.iwakura@gmail.com>
2026-07-27 09:13:00 +0900

    Fix: CustomChannel: Discord channel id load correctly

--- src/main/java/net/unknown/survival/chat/CustomChannels.java
+++ src/main/java/net/unknown/survival/chat/CustomChannels.java
@@ -88,7 +88,7 @@ public class CustomChannels {
                         UUID owner = UUID.fromString(CONFIG.getString("channels." + channelName + ".owner"));
                         Component displayName = GsonComponentSerializer.gson().deserialize(CONFIG.getString("channels." + channelName + ".display_name"));
                         List<UUID> players = CONFIG.getStringList("channels." + channelName + ".players").stream().map(rawUniqueId -> UUID.fromString(rawUniqueId)).toList();
-                        String discordChannelId = CONFIG.contains("discord_channel_id") ? CONFIG.getString("discord_channel_id") : null;
+                        String discordChannelId = CONFIG.contains("channels." + channelName + ".discord_channel_id") ? CONFIG.getString("channels." + channelName + ".discord_channel_id") : null;
                         CHANNELS.put(channelName, new CustomChannel(channelName, displayName, owner, players, discordChannelId));
                     });
                 }
