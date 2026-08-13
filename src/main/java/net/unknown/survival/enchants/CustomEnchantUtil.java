fbde12fdb886bffbf66606f8b9769af6d14c9eb5
Ryuta Iwakura <ryuuta.iwakura@gmail.com>
2026-08-14 00:58:07 +0900

    fix: NPE: CustomEnchantUtil#hasEnchantment

--- src/main/java/net/unknown/survival/enchants/CustomEnchantUtil.java
+++ src/main/java/net/unknown/survival/enchants/CustomEnchantUtil.java
@@ -103,6 +103,7 @@ public class CustomEnchantUtil {
     }
 
     public static boolean hasEnchantment(String loreLine, ItemStack selectedItem) {
+        if (selectedItem == null) return false;
         if (selectedItem.lore() == null) return false;
         List<String> lore = selectedItem.lore().stream().map(LegacyComponentSerializer.legacySection()::serialize).toList();
         return lore.stream().anyMatch(s -> s.startsWith("§7" + loreLine));
